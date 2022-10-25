93c69df6ce995bf280df16b6ff8ef2378ac29a7c
MGLarsenSW <mathias.larsen9@gmail.com>
2022-10-25 12:29:51 +0200

    test

---
+++ P3/src/main/java/org/example/main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.io.IOException;
+
+public class main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World!");
+    }
+}
