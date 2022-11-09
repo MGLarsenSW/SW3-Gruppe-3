68965b8060b3528d7a7fbefca4f189f90c1f407c
MGLarsenSW <mathias.larsen9@gmail.com>
2022-11-09 11:51:04 +0100

    added spring

--- P3/src/main/java/org/example/main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import java.io.IOException;
-
-public class main {
-    public static void main(String[] args) throws IOException {
-        System.out.println("Hello World!");
-    }
-}
