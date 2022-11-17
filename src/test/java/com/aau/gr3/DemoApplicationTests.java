e797efca9a44a3feebac21ba9f2a62c39bb992c9
PrometheusBork <tobiasmerithz@gmail.com>
2022-11-17 12:45:24 +0100

    p3apptest

--- src/test/java/com/aau/gr3/DemoApplicationTests.java
+++ src/test/java/com/aau/gr3/DemoApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class P3ApplicationTests {
 
 	@Test
 	void contextLoads() {
