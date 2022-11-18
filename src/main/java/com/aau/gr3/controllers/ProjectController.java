ac23ffff07971d4330c7708260e91970aeb9daf5
PrometheusBork <tobiasmerithz@gmail.com>
2022-11-18 12:21:53 +0100

    setup of html and bootstrap

--- src/main/java/com/aau/gr3/controllers/ProjectController.java
+++ src/main/java/com/aau/gr3/controllers/ProjectController.java
@@ -1,5 +1,6 @@
 package com.aau.gr3.controllers;
 
+import com.aau.gr3.classes.Project;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ProjectController {
     @GetMapping("/ProjectOverview")
     String getProjects(Model model) {
-        model.addAttribute("Projects", "bliblop" );
+        model.addAttribute("Projects", "AUUUGH");
         return "ProjectOverview";
     }
 }
