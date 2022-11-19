ad4aed843ebacff1ea788cacab2dc3ba3cd51a87
ITPini <marc6571us@gmail.com>
2022-11-19 16:25:13 +0100

    Removed redundant read() method and changed access modifiers

--- src/main/java/com/example/gr3/p3/util/Connection.java
+++ src/main/java/com/example/gr3/p3/util/Connection.java
@@ -9,17 +9,11 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 
 public class Connection {
-    private MongoClient mongoClient;
-    private MongoDatabase database;
-    private MongoCollection<Document> collection;
+    protected MongoClient mongoClient;
+    protected MongoDatabase database;
+    protected MongoCollection<Document> collection;
 
     public Connection() {
-    }
-
-    public MongoCollection<Document> read(String collection){
-        this.database = mongoClient.getDatabase("graintec");
-        this.collection = this.database.getCollection(collection);
-        return this.collection;
     }
     public void close(){
         this.mongoClient.close();
