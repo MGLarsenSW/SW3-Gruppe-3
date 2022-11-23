0c2deb9eec6944fd58dbd7f5d8774473817295a3
ITPini <marc6571us@gmail.com>
2022-11-23 22:18:41 +0100

    Refactored State class

--- src/main/java/com/aau/gr3/classes/State.java
+++ src/main/java/com/aau/gr3/classes/State.java
@@ -3,70 +3,80 @@ package com.aau.gr3.classes;
 import org.bson.types.ObjectId;
 
 public class State extends Supplier{
-    private boolean stateRFI;
-    private boolean stateTender;
-    private boolean stateQA;
-    private boolean stateReminder;
-    private boolean stateQuotation;
-    private boolean stateContract;
+    private boolean RFI;
+    private boolean tender;
+    private boolean QA;
+    private boolean reminder;
+    private boolean quotation;
+    private boolean contract;
 
     public State(ObjectId _id, int pid, String contactPerson, String contactMail,
-                 String supplier, String supply, boolean stateRFI, boolean stateTender,
-                 boolean stateQA, boolean stateReminder, boolean stateQuotation, boolean stateContract){
+                 String supplier, String supply, boolean RFI, boolean tender,
+                 boolean QA, boolean reminder, boolean quotation, boolean contract){
         super(_id, pid, contactPerson, contactMail, supplier, supply);
-        this.stateRFI = stateRFI;
-        this.stateTender = stateTender;
-        this.stateQA = stateQA;
-        this.stateReminder = stateReminder;
-        this.stateQuotation = stateQuotation;
-        this.stateContract = stateContract;
+        this.RFI = RFI;
+        this.tender = tender;
+        this.QA = QA;
+        this.reminder = reminder;
+        this.quotation = quotation;
+        this.contract = contract;
+    }
+    public State(ObjectId _id, int pid, String contactPerson, String contactMail,
+                 String supplier, String supply){
+        super(_id, pid, contactPerson, contactMail, supplier, supply);
+        this.RFI = false;
+        this.tender = false;
+        this.QA = false;
+        this.reminder = false;
+        this.quotation = false;
+        this.contract = false;
     }
 
-    public boolean isStateRFI() {
-        return stateRFI;
+    public boolean isRFI() {
+        return RFI;
     }
 
-    public void setStateRFI(boolean stateRFI) {
-        this.stateRFI = stateRFI;
+    public void setRFI(boolean RFI) {
+        this.RFI = RFI;
     }
 
-    public boolean isStateTender() {
-        return stateTender;
+    public boolean isTender() {
+        return tender;
     }
 
-    public void setStateTender(boolean stateTender) {
-        this.stateTender = stateTender;
+    public void setTender(boolean tender) {
+        this.tender = tender;
     }
 
-    public boolean isStateQA() {
-        return stateQA;
+    public boolean isQA() {
+        return QA;
     }
 
-    public void setStateQA(boolean stateQA) {
-        this.stateQA = stateQA;
+    public void setQA(boolean QA) {
+        this.QA = QA;
     }
 
-    public boolean isStateReminder() {
-        return stateReminder;
+    public boolean isReminder() {
+        return reminder;
     }
 
-    public void setStateReminder(boolean stateReminder) {
-        this.stateReminder = stateReminder;
+    public void setReminder(boolean reminder) {
+        this.reminder = reminder;
     }
 
-    public boolean isStateQuotation() {
-        return stateQuotation;
+    public boolean isQuotation() {
+        return quotation;
     }
 
-    public void setStateQuotation(boolean stateQuotation) {
-        this.stateQuotation = stateQuotation;
+    public void setQuotation(boolean quotation) {
+        this.quotation = quotation;
     }
 
-    public boolean isStateContract() {
-        return stateContract;
+    public boolean isContract() {
+        return contract;
     }
 
-    public void setStateContract(boolean stateContract) {
-        this.stateContract = stateContract;
+    public void setContract(boolean contract) {
+        this.contract = contract;
     }
 }
