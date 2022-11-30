40a8cad8f429a08ab78b8bbdd0a5d157258d8019
GhostMG14 <muadghanim14@gmail.com>
2022-11-30 09:41:27 +0100

    i need help
    i need cancer help, i need you to show me how to do this, i made a clean plate for demonstration purposes

---
+++ src/main/java/com/aau/gr3/controllers/ScoringController.java
@@ -0,0 +1,14 @@
+package com.aau.gr3.controllers;
+
+import com.aau.gr3.classes.Scoring;
+import com.aau.gr3.crud.Read;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+@Controller
+public class ScoringController {
+    @GetMapping("/Scoring")
+    
+
+}
