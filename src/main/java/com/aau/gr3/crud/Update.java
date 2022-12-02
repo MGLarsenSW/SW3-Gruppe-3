666a73fe06947885e2af7489582eb4074b61ff37
Pini <marc6571us@gmail.com>
2022-12-02 16:45:22 +0100

    Implemented start of update CRUD operation

---
+++ src/main/java/com/aau/gr3/crud/Update.java
@@ -0,0 +1,48 @@
+package com.aau.gr3.crud;
+
+import com.aau.gr3.classes.Scoring;
+import com.aau.gr3.classes.State;
+import com.mongodb.client.MongoDatabase;
+import org.bson.types.ObjectId;
+
+public class Update {
+    private MongoDatabase mongoDatabase;
+
+    public Update(MongoDatabase mongoDatabase) {
+        this.mongoDatabase = mongoDatabase;
+    }
+
+    // TODO: Consider refactoring these methods into a single method that takes a generic object as a parameter
+
+    /**
+     * Updates the state of a project
+     * @param id - Supplier ID (ObjectID)
+     * @param state - State object
+     * @return - Returns true if the state was updated successfully otherwise false
+     */
+    public boolean updateSupplierState(ObjectId id, State state) {
+        try {
+            mongoDatabase.getCollection("Supplier").updateOne(new org.bson.Document("_id", id), new org.bson.Document("$set", new org.bson.Document("State", state)));
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * Updates the scoring of a project
+     * @param id - Supplier ID (ObjectID)
+     * @param scoring - Scoring object
+     * @return - Returns true if the scoring was updated successfully otherwise false
+     */
+    public boolean updateSupplierScoring(ObjectId id, Scoring scoring){
+        try {
+            mongoDatabase.getCollection("Supplier").updateOne(new org.bson.Document("_id", id), new org.bson.Document("$set", new org.bson.Document("Scoring", scoring)));
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
